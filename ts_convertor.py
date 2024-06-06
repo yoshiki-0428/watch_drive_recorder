fa3d676a80734544b02ae9930141832c0cd4e73d
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2024-06-06 19:57:38 +0900

    fix

--- ts_convertor.py
+++ ts_convertor.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 import shutil
 
 import ffmpeg
@@ -11,7 +12,7 @@ OUTPUT_DIR = "output"
 FILTER_DURATION = 20
 
 
-class TSFileConvertor:
+class TSFileConverter:
     class TSFileIsEmpty(Exception):
         pass
 
@@ -45,7 +46,7 @@ class TSFileConvertor:
                     ts_rear_files.append(movie_filename)
 
         if len(ts_files) == 0 and len(ts_rear_files) == 0:
-            raise TSFileConvertor.TSFileIsEmpty("No TS files found. Exiting...")
+            raise TSFileConverter.TSFileIsEmpty("No TS files found. Exiting...")
 
         self.ts_files = ts_files
         self.ts_rear_files = ts_rear_files
@@ -57,7 +58,7 @@ class TSFileConvertor:
     def convert(self) -> None:
         print(f"Founded {len(self.ts_files)} TS files. Convert is starting...")
 
-        for date_str, file_list in TSFileConvertor.grouping_files_by_day(
+        for date_str, file_list in TSFileConverter.grouping_files_by_day(
             self.ts_files
         ).items():
             # 1つ以上の場合は ファイルを結合する
@@ -119,90 +120,6 @@ class TSFileConvertor:
 
         return files_by_date
 
-    # def __ts2mp4(self) -> None:
-    #     current = 1
-    #     for input_file in self.ts_files:
-    #         output_file = os.path.join(
-    #             self.output_dir,
-    #             os.path.splitext(os.path.basename(input_file))[0] + ".mp4",
-    #         )
-    #
-    #         # Convert the TS file to MP4
-    #         print(f"{current} / {len(self.ts_files)}")
-    #         print(
-    #             f"File name: {output_file}. Output: {output_file}. Convert is starting. "
-    #         )
-    #         ffmpeg.input(input_file).output(output_file).run(
-    #             overwrite_output=True, quiet=True
-    #         )
-    #         print(f"File name: {output_file}. Convert is end. ")
-    #         current += 1
-
-    # def __group_mp4_files_by_day(self) -> None:
-    #     """
-    #     日ごとに分けてMP4ファイルを結合する
-    #
-    #     :return: None
-    #     """
-    #     files_by_date: Dict[str, List[MovieFilename]] = {}
-    #
-    #     # Group files by creation date
-    #     for _filename in os.listdir(self.output_dir):
-    #         try:
-    #             movie_file = MovieFilename(_filename)
-    #         except MovieFilename.MovieFilenameError as e:
-    #             print(e)
-    #             continue
-    #
-    #         if movie_file.is_file_type("mp4"):
-    #             date_str = movie_file.date
-    #             if date_str not in files_by_date:
-    #                 files_by_date[date_str] = []
-    #
-    #             if movie_file.is_front_camera():
-    #                 files_by_date[date_str].append(movie_file)
-    #
-    #     print(f"Grouped files by date: {files_by_date}")
-    #
-    #     # Combine files by date
-    #     for date_str, file_list in files_by_date.items():
-    #         # 1つの場合は 日付.mp4でoutputからファイル移動をする
-    #         if len(file_list) <= 1:
-    #             shutil.move(
-    #                 os.path.join(self.output_dir, file_list[0].origin),
-    #                 os.path.join(self.combine_output_dir, f"{date_str}.mp4"),
-    #             )
-    #             print(
-    #                 f"Moved {file_list[0]} to {os.path.join(self.output_dir, f'{date_str}.mp4')}"
-    #             )
-    #
-    #         else:
-    #             video_clips = []
-    #             for video in sorted(file_list, key=lambda x: x.time):
-    #                 video_clips.append(
-    #                     VideoFileClip(os.path.join(self.output_dir, video.origin))
-    #                 )
-    #
-    #             try:
-    #                 complete_clip: CompositeVideoClip = concatenate_videoclips(
-    #                     video_clips, method="compose"
-    #                 )
-    #                 complete_clip.write_videofile(
-    #                     os.path.join(self.combine_output_dir, f"{date_str}.mp4"),
-    #                     fps=30,
-    #                     codec="libx264",
-    #                     audio_codec="aac",
-    #                 )
-    #                 print(
-    #                     f"Combined files for {date_str} into {os.path.join(self.output_dir, f"{date_str}.mp4")}"
-    #                 )
-    #
-    #             except Exception as e:
-    #                 print(f"Error combining files for {date_str}: {e}")
-    #
-    #             for video in file_list:
-    #                 os.remove(os.path.join(self.output_dir, video.origin))
-
 
 def convert_ts_file(sd_card_path: str) -> None:
     print(f"Converting TS files in {sd_card_path}...")
@@ -213,10 +130,10 @@ def convert_ts_file(sd_card_path: str) -> None:
             files.append(os.path.join(abs_sd_path, file))
 
     try:
-        convertor: TSFileConvertor = TSFileConvertor(sd_card_path)
+        convertor: TSFileConverter = TSFileConverter(sd_card_path)
         convertor.convert()
 
-    except TSFileConvertor.TSFileIsEmpty as e:
+    except TSFileConverter.TSFileIsEmpty as e:
         print(e)
     except Exception as e:
         print(f"Error: {e}")
